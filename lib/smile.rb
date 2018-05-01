fe36bbfd0797e077b80fc7eb73c692da9434ae14
Alyson T <shtakai@gmail.com>
2018-05-01 15:34:39 +0700

    Add simple way of smile

---
+++ lib/smile.rb
@@ -0,0 +1,7 @@
+class Smile
+  attr_accessor :smile
+
+  def ensmile
+    @smile = "SMILE"
+  end
+end
--- spec/smile_spec.rb
+++ spec/smile_spec.rb
@@ -1,2 +1,12 @@
 require 'spec_helper'
+require_relative '../lib/smile'
 
+RSpec.describe Smile do
+  let(:smile) { described_class.new }
+  describe '#ensmile' do
+    it 'returns "SMILE"' do
+      expect(smile.ensmile).to eq 'SMILE'
+      expect(smile.smile).to eq 'SMILE'
+    end
+  end
+end
