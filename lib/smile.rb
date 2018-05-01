9e72bf32a333ee2a365243576d32704c55cbf6b5
Alyson T <shtakai@gmail.com>
2018-05-01 15:53:02 +0700

    Add finised style

--- lib/smile.rb
+++ lib/smile.rb
@@ -1,7 +1,14 @@
 class Smile
+  SmileError = Class.new(StandardError)
+
   attr_accessor :smile
 
-  def ensmile
-    @smile = "SMILE"
+  def ensmile(cry: false)
+    @smile ||= begin
+      raise SmileError if cry
+      "SMILE"
+    rescue SmileError
+      "CRY"
+    end
   end
 end
--- spec/smile_spec.rb
+++ spec/smile_spec.rb
@@ -5,8 +5,15 @@ RSpec.describe Smile do
   let(:smile) { described_class.new }
   describe '#ensmile' do
     it 'returns "SMILE"' do
+      expect(smile.smile).to be_nil
       expect(smile.ensmile).to eq 'SMILE'
       expect(smile.smile).to eq 'SMILE'
+    end
+
+    it 'returns "CRY"' do
+      expect(smile.smile).to be_nil
+      expect(smile.ensmile(cry: true)).to eq 'CRY'
+      expect(smile.smile).to eq 'CRY'
     end
   end
 end
